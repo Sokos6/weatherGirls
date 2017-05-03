2d87c57b7098238bcebf6d10c0aced0bef09c8e7
Sokos6 <will.sokolowski@gmail.com>
2017-05-03 17:32:30 -0400

    basic data request

--- app/src/main/java/com/inmotionhosting/weathergirls/MainActivity.java
+++ app/src/main/java/com/inmotionhosting/weathergirls/MainActivity.java
@@ -2,12 +2,42 @@ package com.inmotionhosting.weathergirls;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String apiKey = "3a84bcb8948b15fab71e9a1b004fdadb";
+        double latitude = 37.8267;
+        double longitude = -122.4233;
+
+        String forecastUrl = "https://api.darksky.net/forecast/" + apiKey + "/" + latitude + "," + longitude;
+
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(forecastUrl).build();
+
+        Call call = client.newCall(request);
+
+        try {
+            Response response = call.execute();
+            if (response.isSuccessful()) {
+                Log.v(TAG, response.body().string());
+            }
+        } catch (IOException e) {
+            Log.e(TAG, "Exception Caught: ", e);
+        }
     }
 }
