bf59a8a944834aca2d1dbf0181e1df279542261b
Sokos6 <will.sokolowski@gmail.com>
2017-05-05 16:45:15 -0400

    json returned

--- app/src/main/java/com/inmotionhosting/weathergirls/MainActivity.java
+++ app/src/main/java/com/inmotionhosting/weathergirls/MainActivity.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import java.io.IOException;
 
 import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -30,14 +31,23 @@ public class MainActivity extends AppCompatActivity {
         Request request = new Request.Builder().url(forecastUrl).build();
 
         Call call = client.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
 
-        try {
-            Response response = call.execute();
-            if (response.isSuccessful()) {
-                Log.v(TAG, response.body().string());
             }
-        } catch (IOException e) {
-            Log.e(TAG, "Exception Caught: ", e);
-        }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try {
+                    if (response.isSuccessful()) {
+                        Log.v(TAG, response.body().string());
+                    }
+                } catch (IOException e) {
+                    Log.e(TAG, "Exception Caught: ", e);
+                }
+            }
+        });
+
     }
 }
